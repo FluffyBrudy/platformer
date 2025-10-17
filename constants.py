f2a83abdf4f0cc36e7203ede0c9f3db2e666e5c9
rudy <fluffyrudy@proton.me>
2025-10-17 13:05:49 +0545

    update constants

--- constants.py
+++ constants.py
@@ -1,3 +1,17 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+BASE_PATH = Path(__file__).parent
+ASSETS_PATH = BASE_PATH / "assets" / "data" / "images"
+
+
 FPS = 60
 SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 700
+SCREEN_HEIGHT = 500
+BASE_SPEED = 100
+
+
+@dataclass
+class Color:
+    BG_COLOR = (14, 219, 248, 255)
+    BLACK = (0, 0, 0, 255)
